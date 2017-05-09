24c386edcb496aeae70b933420d7b4c8a07dc38e
Administrator <757372373@qq.com>
2017-05-09 14:34:41 +0800

    添加Get Set

--- app/src/main/java/map/jiyun/com/oschina/Person.java
+++ app/src/main/java/map/jiyun/com/oschina/Person.java
@@ -9,4 +9,45 @@ public class Person {
     private String name;
     private String sex;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
+    public Person() {
+    }
+
+    public Person(int age, String name, String sex) {
+        this.age = age;
+        this.name = name;
+        this.sex = sex;
+    }
 }
